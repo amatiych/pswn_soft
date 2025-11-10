249e37470d6d52e8970cf6bd671310efbf38d62c
aleksey matiychenko <aleksey@purpleswansoftware.com>
2025-11-10 14:58:18 -0500

    Comments added

--- src/purple_swan/analytics/var/var_engine.py
+++ src/purple_swan/analytics/var/var_engine.py
@@ -4,14 +4,25 @@ import numpy as np
 import json
 from purple_swan.core.timer import timed
 
+"""
+    Simplified Version of VaR Engine.
+    It calculates the following measures:
+        1. var - value at risk at each confidence level (e.g. 0.95,0.99 etc)
+        2. es - expeted shortfall at the same confidence level 
+        3. marginal var - var that accounts for removal of one asset at a time
+        4. incremental var - each assets contribution to var.
+
+"""
 class VaR:
-    def __init__(self,*, ci, var, k, es,idx,marginal_var=[]):
+    def __init__(self,*, ci, var, k, es,idx,
+                 marginal_var=[], incremental_var=[]):
         self.ci = float(ci)
         self.var = float(var)
         self.es = float(es)
         self.var_index = int(k)
         self.tail_indexes = list(idx)
-        self.marginal_var = marginal_var
+        self.marginal_var = marginal_var,
+        self.incremental_var = incremental_var
 
 
     def __repr__(self):
@@ -72,20 +83,23 @@ class VarEngine:
             es = np.mean([P[i] for i in idx[:k]])
             var = -1 * float(vars_[i])
 
-            #marginal calc
+            #broadcasting the vectors removes the need for loops
             P_wo = P[:, None] - self.R * self.W
             kth_vals = np.partition(P_wo, k, axis=0)[k, :]  # (N,)
             var_wo = -kth_vals  # VaR without each name
             mar_var = var - var_wo  # (N,)
+            var_idx = int(idx[k])
+            inc_var = self.R[var_idx,:] * self.W
 
             results.append(
                 VaR(
                     ci = float(ci),
                     var = var,
-                    k = int(idx[k]),
+                    k = var_idx,
                     es = es,
                     idx = [int(i) for i in idx[:k]],
-                    marginal_var = [float(mv) for mv in mar_var]
+                    marginal_var = [float(mv) for mv in mar_var],
+                    incremental_var = [float(iv) for iv in inc_var]
                 )
             )
         return results
@@ -98,4 +112,9 @@ if __name__ == "__main__":
     weights = [1.0 / N] * N
     var_e = VarEngine(df_ts, weights)
     res = var_e.calc_var([0.95])
+    res = res[0]
+    var = res.var
+    ivars = res.incremental_var
+    tot_ivar = sum(ivars)
+    print (f"VaR: {var} Tot IvaR: {tot_ivar}")
     print(res)
